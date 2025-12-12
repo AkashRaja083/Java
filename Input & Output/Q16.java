d33d0985f16b78ca2fb38339690c881fd3986902
JebinAkashRaja A <crazyakash059@gmail.com>
2025-12-12 19:16:25 +0530

    Update Q16.java

--- Input & Output/Q16.java
+++ Input & Output/Q16.java
@@ -7,7 +7,8 @@ public class Q16 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         char ch= sc.next().charAt(0);
-        System.out.printf(" \" %c \" ",ch);
+        System.out.printf(" \' %c \' ",ch);
         sc.close();
     }    
+
 }
